80fcec5c3e6943b866633ec23777c0b864403747
Al-Ayaan-Ansari <itsmeayaan730@gmail.com>
2024-07-15 15:25:35 +0530

    completed task 1

--- number_guessing_game/main.cpp
+++ number_guessing_game/main.cpp
@@ -1,2 +1,31 @@
 #include<iostream>
+#include<stdlib.h>
+#include<time.h>
 using namespace std;
+
+
+//this is a number guesssing game that also keep track of no. of tries
+
+
+int main(){
+    srand( (unsigned) time(NULL));
+    int guess=0;
+    int random=(rand()%100)+1;
+    cout<<"guess the no. from 1 to 100"<<endl;
+    //it counts the no. of tries.
+    int count=0;
+    do{
+        count++;
+        cin>>guess;
+        if(random>guess){
+            cout<<"guess HIGHER"<<endl;
+        }
+        else if(random<guess){
+            cout<<"guess LOWER"<<endl;
+        }
+        else{
+            cout<<"YOU GUESSED CORRECT!! in "<<count<<" no. of tries"<<endl;
+        }
+        
+    }while(guess!=random);
+}
