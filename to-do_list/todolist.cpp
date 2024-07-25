3a6399aaaed86145c7db53904aced1fd023b8f42
Al-Ayaan-Ansari <itsmeayaan730@gmail.com>
2024-07-25 17:12:39 +0530

    completed task 4

--- to-do_list/todolist.cpp
+++ to-do_list/todolist.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
-//#include<utility>
 #include <vector>
 using namespace std;
+#define UNDERLINE "\033[4m"
+#define CLOSEUNDERLINE "\033[0m"
 
 void inputTask(vector<pair<string,bool>>&tasks){
     cout<<"enter task\n";
     string task;
+    cin.ignore();
     getline (cin, task);
     tasks.push_back({task,0});
 
@@ -14,20 +16,72 @@ void inputTask(vector<pair<string,bool>>&tasks){
 }
 
 void viewtask(vector<pair<string,bool>>&tasks){
-    cout<<"Sno.          status             tasks\n";
+    cout<<UNDERLINE<<"Sno.           |status             |tasks\n"<<CLOSEUNDERLINE;
     for(int i=0;i<tasks.size();i++){
-        string status;
         if(tasks[i].second)
-            status="completed";
+            cout<<i+1<<".             "<<"|completed"<<"          |"<<tasks[i].first<<endl;
         else{
-            status="pending";
+            cout<<i+1<<".             "<<"|pending"<<"            |"<<tasks[i].first<<endl;
         }
-        cout<<i+1<<"             "<<status<<"             "<<tasks[i].first<<endl;
     }
 }
+
+void marktask(vector<pair<string,bool>> &tasks){
+    int mark;
+    cout<<"mark the task that is completed\n";
+    cin>>mark;
+    tasks[mark-1].second =true;
+}
+
+void Removetask(vector<pair<string,bool>> &tasks){
+    int mark;
+    cout<<"enter the index of the task to be removed\n";
+    cin>>mark;
+    tasks.erase(tasks.begin()+mark-1);
+}   
+
+
+
 int main(){
     vector<pair<string,bool>> tasks;
-    inputTask(tasks);
-    viewtask(tasks);
-    
+    bool start=true;
+
+    while(start){
+        int input;
+        cout<<"1.  Input a task\n";
+        cout<<"2.  View the tasks\n";
+        cout<<"3.  mark to be complete\n";
+        cout<<"4.  Remove a task\n";
+        cout<<"enter -1 to close the program\n";
+        cout<<"enter option: \n";
+        cin>>input;
+        switch (input)
+        {
+        case  1:
+            inputTask(tasks);
+            viewtask(tasks);
+            break;
+        
+        case  2:
+            viewtask(tasks);
+            break;
+        
+        case  3:
+            marktask(tasks);
+            viewtask(tasks);
+            break;
+
+        case  4:
+            Removetask(tasks);
+            viewtask(tasks);
+            break;
+
+        case -1:
+            start = false;
+            break;
+        default:
+            cout<<"please enter carefully\n";
+            break;
+        }
+    }
 }
